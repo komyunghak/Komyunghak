d9724e810667e6be23f2e44f082ddd0f76fce76b
hu-412 <hu-412@hu-412-PC>
2020-07-24 17:10:37 +0900

    7.24 금요일  자습시간

--- src/test/java/org/edu/test/DataSourceTest.java
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -20,48 +20,60 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class DataSourceTest {
 
-   @Inject
-   private DataSource ds;
-   
-   @Inject
-   private IF_MemberService memberService;
-   
-   
-   @Test
-   public void testConnection()throws Exception{
-      Connection con = ds.getConnection();
-      System.out.println("데이터베이스 커넥션 성공 : " + con);
-   }
-   
-   @Test
-   public void testDeleteMember() throws Exception {
-      memberService.deleteMember("user02");
-   }
-   @Test
-   public void testUpdateMember() throws Exception {
-      MemberVO memberVO = new MemberVO();
-      memberVO.setUser_id("user02");
-      memberVO.setUser_pw("1234");
-      memberVO.setUser_name("고명학");
-      memberVO.setEmail("audgkr7836@naver.com");
-      memberVO.setPoint(100);
-      memberVO.setEnabled(true);
-      memberVO.setLevels("ROLE_ADMIN");
-      memberService.updateMember(memberVO);
-   	  }
-   
-   
-   @Test
-   public void testSelectMember() throws Exception {
-      System.out.println("회원리스트 입니다.");
-      PageVO pageVO = new PageVO();
-      pageVO.setPage(1);
-      pageVO.setPerPageNum(10);
-      List<MemberVO> list = memberService.selectMember(pageVO);
-      System.out.println(list);
-     /* for(MemberVO vo:list) {
-         System.out.println("사용자아이디 : " + vo.getUser_id());
-         System.out.println("사용자이메일 : " + vo.getEmail());
-      }*/
-   }
+	@Inject
+	private DataSource ds;
+	
+	@Inject
+	private IF_MemberService memberService;
+	
+	@Test
+	public void testConnection() throws Exception {
+		Connection con = ds.getConnection();
+		System.out.println("데이터베이스 커넥션 성공 : " + con);
+	}
+	
+	@Test
+	public void testDeleteMember() throws Exception {
+		memberService.deleteMember("user02");
+	}
+	
+	@Test
+	public void testUpdateMember() throws Exception {
+		MemberVO memberVO = new MemberVO();
+		memberVO.setUser_id("user02");
+		memberVO.setUser_pw("1234");
+		memberVO.setUser_name("김일국");
+		memberVO.setEmail("kimilguk@test.com");
+		memberVO.setPoint(100);
+		memberVO.setEnabled(true);
+		memberVO.setLevels("ROLE_ADMIN");
+		memberService.updateMember(memberVO);
+	}
+	
+	@Test
+	public void testInsertMember() throws Exception {
+		MemberVO memberVO = new MemberVO();
+		memberVO.setUser_id("kimilguk");
+		memberVO.setUser_pw("1234");
+		memberVO.setUser_name("김일국");
+		memberVO.setEmail("kimilguk@test.com");
+		memberVO.setPoint(100);
+		memberVO.setEnabled(true);
+		memberVO.setLevels("ROLE_USER");
+		memberService.insertMember(memberVO);
+	}
+	
+	@Test
+	public void testSelectMember() throws Exception {
+		System.out.println("회원리스트 입니다.");
+		PageVO pageVO = new PageVO();
+		pageVO.setPage(1);
+		pageVO.setPerPageNum(10);
+		List<MemberVO> list = memberService.selectMember(pageVO);
+		System.out.println(list);
+		/*for(MemberVO vo:list) {
+			System.out.println("사용자아이디 : " + vo.getUser_id());
+			System.out.println("사용자이메일 : " + vo.getEmail());
+		}*/
+	}
 }
